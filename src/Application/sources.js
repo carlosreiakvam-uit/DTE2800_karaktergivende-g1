48e440a8d10a2f715c1d5f7ff6a1cb62d16cc327
carlos reiakvam <cre032@uit.no>
2022-11-07 14:18:26 +0100

    Remove redundant imports

--- src/Application/sources.js
+++ src/Application/sources.js
@@ -25,46 +25,6 @@ export default [
         path: 'textures/dirt/normal.jpg'
     },
     {
-        name: 'horizontalWireInDrums',
-        type: 'texture',
-        path: 'textures/drums/hor_drums.png'
-    },
-    {
-        name: 'wireInDrums',
-        type: 'texture',
-        path: 'textures/drums/drums.jpg'
-    },
-    {
-        name: 'hook',
-        type: 'gltfModel',
-        path: 'models/hook.glb'
-    },
-    {
-        name: 'vehicle',
-        type: 'gltfModel',
-        path: 'models/vehicle.glb'
-    },
-    {
-        name: 'rustColorTexture',
-        type: 'texture',
-        path: 'textures/rust/color.jpg'
-    },
-    {
-        name: 'headlightGlassTexture',
-        type: 'texture',
-        path: 'textures/headlightGlass/headlightGlass.jpg'
-    },
-    {
-        name: 'rustNormalTexture',
-        type: 'texture',
-        path: 'textures/rust/normal.jpg'
-    },
-    {
-        name: 'rustAlphaTexture',
-        type: 'texture',
-        path: 'textures/rust/alpha.jpg'
-    },
-    {
         name: "blackDirtyTexture",
         type: "texture",
         path: "textures/blackDirtyMetal/texture.jpg"
