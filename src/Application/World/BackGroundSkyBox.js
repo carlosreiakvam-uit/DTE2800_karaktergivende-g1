6afa47535a15f05ec7bb90cd6d900e9794e1ce78
Patrick Dahl <pda020@uit.no>
2022-11-15 16:05:35 +0100

    #skybox background impl

---
+++ src/Application/World/BackGroundSkyBox.js
@@ -0,0 +1,26 @@
+import * as THREE from 'three'
+import Application from '../Application.js'
+
+export async function addSkyBox(scene) {
+
+    const cubeTextureLoader = new THREE.CubeTextureLoader();
+    // Mer rekkfølgen:
+    const environmentMapTexture = await cubeTextureLoader.load([
+        '../../../textures/cubemaps/GardenNook/px.png',   //positiv x (høyre)
+        '../../../textures/cubemaps/GardenNook/nx.png',   //negativ x (venstre)
+        '../../../textures/cubemaps/GardenNook/py.png',   //positiv y (opp)
+        '../../../textures/cubemaps/GardenNook/ny.png',   //negativ y (ned)
+        '../../../textures/cubemaps/GardenNook/pz.png',   //positiv z (ut)
+        '../../../textures/cubemaps/GardenNook/nz.png',   //negativ z (inn)
+    ]);
+    scene.background = environmentMapTexture;
+
+    const standardMaterial = new THREE.MeshStandardMaterial();
+    standardMaterial.metalness =0.7;
+    standardMaterial.roughness=0.03;
+    standardMaterial.envMap = environmentMapTexture;
+}
+
+
+
+
