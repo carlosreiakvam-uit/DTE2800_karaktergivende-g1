7a02f9c75bd53341027e5457ed1146c6d51d20cc
Patrick Dahl <pda020@uit.no>
2022-11-18 09:51:56 +0100

    #bugfix

--- src/Application/World/StaticEnemies/Lava.js
+++ src/Application/World/StaticEnemies/Lava.js
@@ -79,12 +79,12 @@ export default class Lava {
         application.scene.add(this.lavaMesh);
 
         let width = this.lavaMesh.geometry.parameters.width;
-        let height = 0.4
+        let height = 0.4;
         let depth = this.lavaMesh.geometry.parameters.height;
 
         let shape = new Ammo.btBoxShape(new Ammo.btVector3(width / 2, height / 2, depth / 2));
         //let shape = new Ammo.btStaticPlaneShape(new Ammo.btVector3(width / 2, height / 2, depth / 2));
-#        this.rigidBody = this.application.physics.createRigidBody(shape, this.lavaMesh, 0.7, 0.8, this.position, 100000);
+        this.rigidBody = this.application.physics.createRigidBody(shape, this.lavaMesh, 0.7, 0.8, this.position, 100000);
         this.lavaMesh.userData.physicsBody = this.rigidBody;
         this.application.physics.world.addRigidBody(this.rigidBody, this.application.physics.COL_GROUP_BOX, this.application.physics.COL_GROUP_BOX | this.application.physics.COL_GROUP_PLANE);
         this.application.physics.rigidBodies.push(this.lavaMesh);
