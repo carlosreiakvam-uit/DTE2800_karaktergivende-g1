d9b5f57bf3a2a5bd456cc3e276f23a01f88bfdbc
Patrick Dahl <pda020@uit.no>
2022-11-26 12:14:58 +0100

    #merge

--- src/Application/World/Player/Player.js
+++ src/Application/World/Player/Player.js
@@ -208,7 +208,6 @@ export default class Player {
                         //     this.application.audio.point.play();
                         // }
 
-
                         console.log("contact:", contactBody.threeMesh.name)
                         if (typeof contactBody.threeMesh.collisionResponse === 'function')
                             contactBody.threeMesh.collisionResponse(contactBody.threeMesh)
